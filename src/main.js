3d2352311f97d45f918b5986d1269c30617e91f7
vidur-believintech <vidur@believintech.com>
2021-02-02 16:41:39 +0530

    desktop navigation completed

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
-// import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope,faPhoneAlt } from '@fortawesome/free-solid-svg-icons'
+import { faFontAwesome,faFacebook,faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add()
+library.add(faFontAwesome,faFacebook,faLinkedin,faEnvelope,faPhoneAlt)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
